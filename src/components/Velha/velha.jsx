db622152c1f17f9df1ea3e1ecaf6607215828ca6
AugustoGAMiotti <agammiotti@gmail.com>
2024-06-14 20:44:57 -0300

    Commit final

--- src/components/Velha/velha.jsx
+++ src/components/Velha/velha.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./velha.css"
 
 function Velha() {
@@ -8,49 +8,56 @@ function Velha() {
         '', '', ''
     ])
 
-    const[gameover, setGameover] = useState(false)
-    const[jogadorDaVez, setJogadorDaVez] = useState('X')
+    const [gameover, setGameover] = useState(false)
+    const [jogadorDaVez, setJogadorDaVez] = useState('X')
+    const [status, setStatus] = useState('Vez do Jogador X')
+
+    useEffect(() => {
+        ganhador()
+    }, [jogadas])
 
     function verificaCelulas(c1, c2, c3) {
         let ganhou =
             (jogadas[c1] != '' && jogadas[c2] != '' && jogadas[c3] != '' &&
-            jogadas[c1] == jogadas[c2] && jogadas[c2] == jogadas[c3])
+                jogadas[c1] == jogadas[c2] && jogadas[c2] == jogadas[c3])
 
         if (ganhou)
-           <label>jogador {jogadorDaVez} ganhou</label>
+            setStatus(`Jogador ${jogadas[c1]} Ganhou`)
 
         return ganhou
     }
+
     function ganhador() {
         if ((verificaCelulas(0, 1, 2)) || (verificaCelulas(0, 3, 6)) || (verificaCelulas(0, 4, 8))
             || (verificaCelulas(3, 4, 5)) || (verificaCelulas(6, 7, 8)) || (verificaCelulas(1, 4, 7))
             || (verificaCelulas(2, 5, 8)) || (verificaCelulas(2, 4, 6))) {
-            avaliableGame = 'false'
-            alert('fim de jogo')
+            setGameover(true)
+
+        } else {
+            const totalDeJogadas = jogadas.filter(x => x != '').length
+            if (totalDeJogadas == 9) {
+                setGameover(true)
+                setStatus("Deu velha")
+            }
         }
-        if (jogadas == 'O') {
-            'Jogador X ganhou'
-        } else if (jogadas == 'X') {
-            'Jogador O ganhou'
-        }
+
     }
 
     function playerMove(index) {
 
         if (jogadas[index] == '' && gameover == false) {
-            
+
             let novasJogagadas = [...jogadas]
             novasJogagadas[index] = jogadorDaVez
             setJogadas(novasJogagadas)
 
             if (jogadorDaVez == 'X') {
                 setJogadorDaVez('O')
-            }
-            else {
+                setStatus('Vez do Jogador O')
+            } else {
                 setJogadorDaVez('X')
+                setStatus('Vez do Jogador X')
             }
-
-            ganhador()
 
         }
 
@@ -65,12 +72,12 @@ function Velha() {
                 {jogadas.map((item, index) => (
                     <button
                         className="buttonBoard"
-                        onClick={()=>{playerMove(index)}}
+                        onClick={() => { playerMove(index) }}
                     >{item}</button>
                 ))}
             </div>
             <label id="status">
-                Vez do jogador {jogadorDaVez}
+                {status}
             </label>
         </>
     )
